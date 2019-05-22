56f1980eb5e6d22c324cb11472d6e8b40553f5c1
Sujoy Saha <sujoys023@gmail.com>
2019-05-22 23:49:37 +0530

    Updates

---
+++ precache-manifest.057e4a7544436b282842692d64333620.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5563c6ceebc66bf5d3f",
+    "url": "/pwa/static/css/main.c0a3326b.chunk.css"
+  },
+  {
+    "revision": "c5563c6ceebc66bf5d3f",
+    "url": "/pwa/static/js/main.b304fb46.chunk.js"
+  },
+  {
+    "revision": "9326abcbfc116d8e730f",
+    "url": "/pwa/static/js/runtime~main.ff6970bd.js"
+  },
+  {
+    "revision": "f5e2148df906bed5a208",
+    "url": "/pwa/static/js/2.4a5360e4.chunk.js"
+  },
+  {
+    "revision": "e8b40d8ec42f196132ae7df4fb17c43a",
+    "url": "/pwa/index.html"
+  }
+];
